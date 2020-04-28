b00eefeba6b0b83364f58ae1e063722d08987f4c
Martin Georgiev <mgeorgiev9611@abv.bg>
2020-04-28 16:34:33 +0100

    Updates

---
+++ precache-manifest.a1ce960b4f16789178c62bfc218c036f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16f3adb684c51373b798c0ed4b46c5d3",
+    "url": "/Library/index.html"
+  },
+  {
+    "revision": "1204424d633f1c009da4",
+    "url": "/Library/static/css/main.07cb61db.chunk.css"
+  },
+  {
+    "revision": "069a380ab2e00018cc3d",
+    "url": "/Library/static/js/2.5742bf19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Library/static/js/2.5742bf19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1204424d633f1c009da4",
+    "url": "/Library/static/js/main.4ca54362.chunk.js"
+  },
+  {
+    "revision": "3d15d294abca62c27a91",
+    "url": "/Library/static/js/runtime-main.72f14d18.js"
+  },
+  {
+    "revision": "cb21a76e9e1f9ac1acf2ec43759052f0",
+    "url": "/Library/static/media/unnamed.cb21a76e.jpg"
+  }
+]);
